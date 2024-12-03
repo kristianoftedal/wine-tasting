0f25dbdb9e994724b02a4f47fbfda0a56bc5ab26
kristianoftedal <oftedal.kristian@gmail.com>
2024-12-03 14:57:40 +0100

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -49,23 +49,23 @@ export default function Home() {
 
   return (
     <>
-      <header>
-        <nav>
-          <button className="circle transparent">
-            <i>arrow_back</i>
-          </button>
-          <h5 className="max">vinklubb</h5>
-          <button className="circle transparent">
-            <i>attach_file</i>
-          </button>
-          <button className="circle transparent">
-            <i>today</i>
-          </button>
-          <button className="circle transparent">
-            <i>more_vert</i>
-          </button>
-        </nav>
-      </header>
+    <header className="primary-container">
+  <nav>
+    <button className="circle transparent">
+      <i>arrow_back</i>
+    </button>
+    <h5 className="max">Vinklubb</h5>
+    <button className="circle transparent">
+      <i>attach_file</i>
+    </button>
+    <button className="circle transparent">
+      <i>today</i>
+    </button>
+    <button className="circle transparent">
+      <i>more_vert</i>
+    </button>
+  </nav>
+</header>
    <main className="responsive max">
       <SelectedFlavors selectedFlavors={selectedFlavors} onFlavorClick={handleFlavorClick} />
 
