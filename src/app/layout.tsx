2383fd07a94c54311bd7e9eebf302f0ffddef215
kristianoftedal <oftedal.kristian@gmail.com>
2024-12-03 14:57:24 +0100

    Update layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,23 +1,11 @@
 import type { Metadata } from "next";
-import localFont from "next/font/local";
 import "./globals.css";
 import "beercss";
 import "material-dynamic-colors";
 
-const geistSans = localFont({
-  src: "./fonts/GeistVF.woff",
-  variable: "--font-geist-sans",
-  weight: "100 900",
-});
-const geistMono = localFont({
-  src: "./fonts/GeistMonoVF.woff",
-  variable: "--font-geist-mono",
-  weight: "100 900",
-});
-
 export const metadata: Metadata = {
-  title: "Wine flavor selector",
-  description: "Get a list of possible tastes",
+  title: "Vinklubb",
+  description: "lag en vinklubb idag!",
 };
 
 export default function RootLayout({
@@ -27,7 +15,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={`${geistSans.variable} ${geistMono.variable}`}>
+      <body className="dark">
         {children}
       </body>
     </html>
