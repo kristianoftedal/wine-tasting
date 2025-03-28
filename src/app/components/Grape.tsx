b9cab1a19009f3eed3dc5698f401ae88b30e95dd
kristianoftedal <oftedal.kristian@gmail.com>
2025-03-28 20:07:20 +0100

    fix

--- src/app/components/Grape.tsx
+++ src/app/components/Grape.tsx
@@ -1,24 +1,28 @@
-export const Grape: React.FC<Props> = ({ grape }) => {
-  const [MDXContent, setMDXContent] = useState(null);
+// import { MDXProvider } from "@mdx-js/react";
+// import dynamic from "next/dynamic";
+// import React, { useState } from "react";
 
-  const slug = grape.match(/[a-zA-Z]+/)[0];
+// export const Grape: React.FC<Props> = ({ grape }) => {
+//   const [MDXContent, setMDXContent] = useState(null);
 
-  const loadMDX = async () => {
-    const MDXComponent = dynamic(() => import(slug));
-    setMDXContent(MDXComponent);
-  };
-  return (
-    <button
-      className="chip round large"
-      onClick={async () => await loadMDX()}>
-      <p>{grape}</p>
-      <div className="tooltip bottom max large-space">
-        {MDXContent && (
-          <MDXProvider>
-            <MDXContent />
-          </MDXProvider>
-        )}
-      </div>
-    </button>
-  );
-};
+//   const slug = grape.match(/[a-zA-Z]+/)[0];
+
+//   const loadMDX = async () => {
+//     const <MDXComponent></MDXComponent> = dynamic(() => import(slug));
+//     setMDXContent(MDXComponent);
+//   };
+//   return (
+//     <button
+//       className="chip round large"
+//       onClick={async () => await loadMDX()}>
+//       <p>{grape}</p>
+//       <div className="tooltip bottom max large-space">
+//         {MDXContent && (
+//           <MDXProvider>
+//             <MDXContent />
+//           </MDXProvider>
+//         )}
+//       </div>
+//     </button>
+//   );
+// };
