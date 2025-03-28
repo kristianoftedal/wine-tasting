864a63ff9ec7fff14b3f1cb01da5ff72638395c1
kristianoftedal <oftedal.kristian@gmail.com>
2025-03-28 12:19:44 +0100

    as string

--- src/app/gruppe/[id]/arrangement/opprett/page.tsx
+++ src/app/gruppe/[id]/arrangement/opprett/page.tsx
@@ -22,7 +22,7 @@ async function createEvent(formData: FormData): Promise<EventDocument> {
   const name = formData.get('name') as string;
   const description = formData.get('name') as string;
   const date = parseISO(formData.get('date') as string);
-  const wines = formData.getAll('wines') as Wine[];
+  const wines = formData.getAll('wines') as string[];
   const group = formData.get('group') as ObjectId;
 
   const event = new Event({
