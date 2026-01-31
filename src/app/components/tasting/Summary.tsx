4997dadbf2875555c1456eb070cfc294f3d65b54
kristianoftedal <oftedal.kristian@gmail.com>
2026-01-31 15:55:14 +0100

    fix

--- src/app/components/tasting/Summary.tsx
+++ src/app/components/tasting/Summary.tsx
@@ -314,11 +314,6 @@ export const Summary: React.FC = () => {
             )}
 
             <div className={styles.summaryRow}>
-              <div className={styles.summaryLabel}>Karakter</div>
-              <div className={styles.summaryValue}>{tastingState.karakter}</div>
-            </div>
-
-            <div className={styles.summaryRow}>
               <div className={styles.summaryLabel}>Alkohol</div>
               <div className={styles.summaryValue}>{tastingState.alkohol}%</div>
             </div>
@@ -334,6 +329,10 @@ export const Summary: React.FC = () => {
                 <div className={styles.summaryValue}>{tastingState.egenskaper}</div>
               </div>
             )}
+            <div className={styles.summaryRow}>
+              <div className={styles.summaryLabel}>Karakter</div>
+              <div className={styles.summaryValue}>{tastingState.karakter}</div>
+            </div>
           </div>
 
           <div className={styles.comparisonToggle}>
@@ -444,13 +443,6 @@ export const Summary: React.FC = () => {
             )}
 
             <div className={styles.tableRow}>
-              <div className={styles.attributeName}>Karakter</div>
-              <div className={styles.attributeValue}>{tastingState.karakter}</div>
-              <div className={styles.attributeValue}>-</div>
-              <div className={styles.scoreValue}>-</div>
-            </div>
-
-            <div className={styles.tableRow}>
               <div className={styles.attributeName}>Alkohol</div>
               <div className={styles.attributeValue}>{tastingState.alkohol}%</div>
               <div className={styles.attributeValue}>{wine?.content?.traits?.[0]?.readableValue}</div>
@@ -462,6 +454,12 @@ export const Summary: React.FC = () => {
               <div className={styles.attributeValue}>{tastingState.pris} kr</div>
               <div className={styles.attributeValue}>{wine?.price} kr</div>
               <div className={styles.scoreValue}>{scores.pris}%</div>
+            </div>
+            <div className={styles.tableRow}>
+              <div className={styles.attributeName}>Karakter</div>
+              <div className={styles.attributeValue}></div>
+              <div className={styles.attributeValue}>-</div>
+              <div className={styles.scoreValue}>{tastingState.karakter}</div>
             </div>
           </div>
 
